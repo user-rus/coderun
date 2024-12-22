51ee542f8fbaa2f60e1941019055c0b47d6a976b
bondarenko <bondarenko.dev@yandex.ru>
2024-12-22 11:10:00 +0300

    1. Средний элемент

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import solution.N1_MiddleElement;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        reader.close();
+        writer.close();
+    }
+}
---
+++ src/solution/N1_MiddleElement.java
@@ -0,0 +1,22 @@
+package solution;
+
+public class N1_MiddleElement {
+    public static Integer middleElement(String[] stringNumbers) {
+        int[] ints = new int[stringNumbers.length];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = Integer.parseInt(stringNumbers[i]);
+        }
+        int max = 0;
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints.length; j++) {
+                if (i == j) continue;
+                if (ints[i] < ints[j]) {
+                    max = ints[j];
+                    ints[j] = ints[i];
+                    ints[i] = max;
+                }
+            }
+        }
+        return ints[1];
+    }
+}
